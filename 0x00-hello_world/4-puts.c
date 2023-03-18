9ef7f031f59687cc8ae14e62d3c507364c4d7afc
ndizihiweeric <ericndizihi2020@gmail.com>
2023-03-18 10:12:51 -0700

    commit for puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - entry point
- * return: always 0 (success)
+ * main - main entry point
+ * Return: always 0 (success)
  */
 int main(void)
 {       puts("\"Programming is like building a multilingual puzzle");
