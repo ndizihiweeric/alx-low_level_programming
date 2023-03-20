e9b4746696ec338bfb8e2ecdbca3f186df63c266
ndizihiweeric <ericndizihi2020@gmail.com>
2023-03-20 13:51:26 -0700

    commit for task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
